2026e40a35d7769adbd2c6f878744f8a8eb59de0
Manuel Gerdel <122245905+manuelgerdel@users.noreply.github.com>
2023-06-29 23:35:24 +0000

    Last version of excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//First variables
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//Function
+function generator(who, action, what, when) {
+  const randomWho = Math.floor(Math.random() * who.length);
+  const randomAction = Math.floor(Math.random() * action.length);
+  const randomWhat = Math.floor(Math.random() * what.length);
+  const randomWhen = Math.floor(Math.random() * when.length);
+  return `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Final variables and html link
+let excuseGenerator = generator(who, action, what, when);
+const htmlText = document.getElementById("heading");
+htmlText.innerText = excuseGenerator;
+
+//console.log(excuseGenerator);
+
+
+
+
